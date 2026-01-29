a8ccac7db7ccab660d41838fa18f0e06f5f8108a
klam912 <lamkenny912@gmail.com>
2026-01-29 16:13:41 +0000

    rename questions into prompts. still need to do the homework questions

--- hw1.R
+++ hw1.R
@@ -1,10 +1,10 @@
 # Github repo URL: https://github.com/klam912/homework1_ENVST325
 
-# Q1
+# Prompt 1
 snow_depths_in <- c(2.5, 3, 5, 4.5)
 snow_depths_cm <- snow_depths_in * 2.54
 
-# Q2
+# Prompt 2
 names <- c("Mount Haystack", "Mount Skylight", "Dix Mountain", "Gray Peak")
 elevation <- c(4960, 4926, 4857, 4840)
 climb_ascent <- c(3570, 4265, 2800, 4178)
@@ -18,7 +18,7 @@ high_peaks <- data.frame(
   check.names = FALSE
 )
 
-# Q3
+# Prompt 3
 fahrenheit_converter <- function(celsius) {
   return ((celsius * (9/5)) + 32)
 }
